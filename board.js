2745772f4d8c4022498f7eb6de6ec19fc3aa0db0
Jameson sandler <108235294+J-sandler@users.noreply.github.com>
2023-01-05 12:00:12 -0500

    Update board.js
    Added piece and pawn logic

--- board.js
+++ board.js
@@ -10,9 +10,12 @@ document.addEventListener("dragover", event => {
 document.addEventListener("drop", event => {
   const move=document.getElementById("move");
   if(event.target.id[0]!=draggedRef.id[0]) {
-    if(move.className[0]!=draggedRef.id[0]||!isvalid(draggedRef.id.slice(1),event.target.id.slice(1),draggedRef.id.slice(1))) {
-      return;
-    }
+    if(move.className[0]!=draggedRef.id[0]||!isvalid(
+    draggedRef.parentNode.id.slice(1),
+    event.target.id.slice(1),draggedRef.id.slice(1),
+    event.target.id[0]!='e',
+    event
+    )) {return;}
 
     event.preventDefault();
     movePiece(event);
@@ -57,6 +60,82 @@ function removeHighlights() {
     })
 }
 
-function isvalid(from, to, piece) {
+function isvalid(from, to, piece, is_capture, event) {
+  let color=draggedRef.id[0];
+  //fix to if necessary
+  if (is_capture) {to=event.target.parentNode.id.slice(1);}
+  //establish x and y movement 
+  let y=parseInt(to[1])-parseInt(from[1]);let x=parseInt(to[0],18)-parseInt(from[0],18);
+  x=Math.abs(x);
+
+  //evaluate x and y movement given the piece moving
+  if (piece=="pawn") {
+    let starting_rank=parseInt(from[1]);
+    if (!is_capture) {
+      if (x) {
+        //unless is enpessant
+        return false;
+      }
+    } 
+
+    if (color=="b") {
+      y=-y;
+    }
+
+    if(is_capture) {
+      if (x>1||y!=1||x==0) {
+        return false;
+      }
+    }
+
+    if(y==2) {
+      if(starting_rank!=7&&starting_rank!=2) {
+        return false;
+      }
+    }
+
+    if(y<0||y>2) {
+      return false;
+    }
+  } else if (piece=="bish") {
+    y=Math.abs(y);
+    if(y!=x) {
+      return false;
+    }
+  } else if (piece=="knight") {
+    y=Math.abs(y);
+    let val=((y==2&&x==1)||(x==2&&y==1));
+    if (!val) {
+      return false;
+    } 
+  } else if (piece=="king") {
+    y=Math.abs(y);
+    if(x>1||y>1) {
+      //unless castle is valid
+      return false;
+    }
+  } else if (piece=="queen") {
+    y=Math.abs(y);
+    let val=(y==0||x==0||y==x);
+    if(!val) {
+      return false;
+    }
+  } else if (piece=="rook") {
+    y=Math.abs(y);
+    if(y!=0&&x!=0) {
+      return false;
+    }
+  } else {
+    alert("You broke the game!, How??");
+  }
+  //check if non-knights jump over pieces
+  if (is_capture) {
+    if(event.target.id.slice(1)=="king") {
+      let winner="black";
+      if (color=="w") {winner="white";}
+      alert(winner + " has won the game!");
+      return false;
+    }
+  }
   return true;
 }
